60499ee1c53296e31bce1b80fe65d01fdcdc7bf7
Paul <paulnyoiken@gmail.com>
2022-11-22 18:34:17 +0300

    Completed assignment 3 attempt

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-
-// Write your code here
-
 let num1 = 31
 let num2 = 2
+let multiply = (num1 * num2);
 
-function multiply(num1, num2) {
-    return multiply = (num1 * num2);
-}
-
-multiply(num1, num2)
-
-function random() {
-    return random = Math.floor(Math.random()*100)
-}
-
-random()
+let random = Math.floor(Math.random()*100)
 
 let num3 = 9
 let num4 = 5
+let mod = num3 % num4
 
-function mod(num3, num4) {
-    return mod = num3 % num4
-}
-
-mod(num3, num4)
-
-function max() {
-    return max = Math.max(13,9,19,5,2,20)
-}
-
-max()
+let max = Math.max(13,9,19,5,2,20)
