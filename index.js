b69d8828c52eb2192ee8e435cf968ad10a7645fb
Paul <paulnyoiken@gmail.com>
2022-11-22 18:08:45 +0300

    Completed assignment second attempt

--- index.js
+++ index.js
@@ -3,12 +3,30 @@
 
 let num1 = 31
 let num2 = 2
-let multiply = (num1 * num2);
 
-let random = Math.floor(Math.random()*100)
+function multiply(num1, num2) {
+    return multiply = (num1 * num2);
+}
+
+multiply(num1, num2)
+
+function random() {
+    return random = Math.floor(Math.random()*100)
+}
+
+random()
 
 let num3 = 9
 let num4 = 5
-let mod = num3 % num4
 
-let max = Math.max(13,9,19,5,2,20)
+function mod(num3, num4) {
+    return mod = num3 % num4
+}
+
+mod(num3, num4)
+
+function max() {
+    return max = Math.max(13,9,19,5,2,20)
+}
+
+max()
